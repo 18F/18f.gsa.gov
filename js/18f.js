2d95d1937899cf3ae4f79c2e73723fbb3cfd717d
Amos Stone <amos@amosarts.com>
2014-03-03 14:03:17 -0500

    Adds slideshow to header.

--- js/18f.js
+++ js/18f.js
@@ -16,4 +16,63 @@ $(document).ready(function() {
       // An error occurred contacting tumblr
     }
   });
+
+
+  var $slideshow = $('#slideshow')
+  var $slides = $('#slideshow').find('img');
+  var $photo_credit = $('.photo-credit');
+  var $wrapper = $('.slideshow-wrapper');
+  console.log('width: ' + $wrapper.width())
+  console.log('height: ' + $wrapper.height())
+
+  $slideshow.slidesjs({
+    width: $wrapper.width(),
+    height: $wrapper.height(),
+    start: Math.floor((Math.random()*$slides.length)+1),
+    effect: {
+      fade: {
+        speed: 800
+      }
+    },
+    play: {
+      active: false,
+      auto: true,
+      effect: 'fade'
+    },
+    interval: 2000,
+    callback: {
+      loaded: function(number) {
+        var $slide = $($slides[number-1]);
+        var caption = '<i class="icon-camera"></i> <span>'+$slide.attr('alt')+'</span>';
+        $photo_credit.html(caption).fadeIn();
+      },
+      start: function(number) {
+        $photo_credit.fadeOut();
+      },
+      complete: function(number) {
+        // var $slide = $slides.get(number-1)
+        var $slide = $($slides[number-1]);
+        var caption = '<i class="icon-camera"></i> <span>'+$slide.attr('alt')+'</span>';
+        $photo_credit.html(caption).fadeIn();
+      }
+    }
+  });
+
+  var slideshowResize = function() {
+    var $wrapper = $('.slideshow-wrapper');
+    var h = $wrapper.height();
+    var w = $wrapper.width();
+
+    $slideshow.width(w).height(h);
+    $slideshow.find('.slide').css('min-height',h)
+    $slideshow.find('.slide').css('min-width',w)
+    return true;
+  }
+
+  slideshowResize();
+
+  $(window).resize(function() {
+    slideshowResize();
+  });
 });
+
