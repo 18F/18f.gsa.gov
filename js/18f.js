fc904e0b146931f69e44f5d8836f6d11afa62f62
Joe Polastre <joe@polastre.com>
2014-03-03 15:14:52 +0000

    Incremental Tumblr commit
    Committing what I have so @quepol can test and look at it.
    
    Still needs to be done:
    - keep blog text at a maximum height (snippet using dotdotdot)
    - link tags correctly back to tumblr

--- js/18f.js
+++ js/18f.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // load posts from tumblr
   $.ajax({
-    url: 'http://api.tumblr.com/v2/blog/18fblog.tumblr.com/posts/text?notes_info=true&limit=2&api_key=cA9agkd1WdAsFUFL5iq1Wnn0m4Dmcv5vf5otES3Ou08r2D3Ldu',
+    url: 'http://api.tumblr.com/v2/blog/peacecorps.tumblr.com/posts/text?notes_info=true&limit=3&filter=text&api_key=cA9agkd1WdAsFUFL5iq1Wnn0m4Dmcv5vf5otES3Ou08r2D3Ldu',
     type: 'GET',
     contentType: 'application/json',
     dataType: 'jsonp',
@@ -9,7 +9,21 @@ $(document).ready(function() {
     success: function (result) {
       for (i in result.response.posts) {
         // render post to the page
-        console.log(result.response.posts[i]);
+        var post = result.response.posts[i]
+        console.log(post);
+        $('#blog' + i + ' .blog-title').html(post.title);
+        $('#blog' + i + ' .blog-title').attr('href', post.post_url);
+        $('#blog' + i + ' .blog-date').html(post.date);
+        $('#blog' + i + ' .blog-snippet').html(post.body);
+        var tagHtml = '';
+        for (j in post.tags) {
+          if (j !== 0) {
+            tagHtml += ', ';
+          }
+          tagHtml += '<a href="' + '#' + '">' + post.tags[j] + '</a>';
+        }
+        $('#blog' + i + ' .blog-tags').html(tagHtml);
+        $('#blog' + i).show();
       }
     },
     error: function (e) {
