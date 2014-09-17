2c6a1ec4fa682fc9bf087c66d43a449ecadaa71f
Eric Mill <eric@konklone.com>
2014-09-17 02:23:43 -0400

    moved browser warning widget nto 18f.js

--- assets/js/18f.js
+++ assets/js/18f.js
@@ -22,3 +22,17 @@ else {
   console.log("%cIf you find a bug or want to talk, we're at %c18f@gsa.gov%c and track issues on %chttps://github.com/18f/18f.gsa.gov/issues", styles.medium, styles.medium_bold, styles.medium, styles.medium_link);
   console.log("%cAnd check us out on GitHub generally! We're an %copen source team%c. %chttps://github.com/18f", styles.medium, styles.medium_bold, styles.medium, styles.medium_link);
 }
+
+
+/*
+  Browser warning widget, for IE8.
+*/
+var $buoop = {vs:{i:8,f:10,o:12,s:5}};
+  $buoop.ol = window.onload;
+  window.onload=function(){
+  try {if ($buoop.ol) $buoop.ol();}catch (e) {}
+  var e = document.createElement("script");
+  e.setAttribute("type", "text/javascript");
+  e.setAttribute("src", "//browser-update.org/update.js");
+  document.body.appendChild(e);
+}
