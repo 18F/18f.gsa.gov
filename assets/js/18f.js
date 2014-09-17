642b0895ca8a56343d012a19054b7a5f4b8a6ea2
Eric Mill <eric@konklone.com>
2014-09-17 02:17:57 -0400

    Removed the hydra signup form entirely, and the IE placeholder JS which was the only thing that took advantage of it

--- assets/js/18f.js
+++ assets/js/18f.js
@@ -1,25 +1,3 @@
-
-// IE Form Placeholders
-$(function() {
-  if(!$.support.placeholder) {
-    var active = document.activeElement;
-    $(':text').focus(function () {
-      if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
-        $(this).val('').removeClass('hasPlaceholder');
-      }
-    }).blur(function () {
-      if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
-        $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
-      }
-    });
-    $(':text').blur();
-    $(active).focus();
-    $('form').submit(function () {
-      $(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
-    });
-  }
-});
-
 // friendly console message
 
 // IE needs plaintext, sigh
