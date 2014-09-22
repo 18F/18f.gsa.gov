a4786c8d6980ff574a8de4fc6f712245c168e671
Greg Boone <gregory.boone@gsa.gov>
2014-09-22 13:38:11 -0400

    GitHub API data now cached and only refreshed if api calls remain

--- _plugins/generate-gh-json.rb
+++ _plugins/generate-gh-json.rb
@@ -8,7 +8,6 @@ module Jekyll
       @projects = site.data['projects']
       @gh_url = 'https://api.github.com/repos/' + @gh_org
       @gh_data = {}
-      binding.pry
       i = 0
       for project in @projects
         if rate() >= 1
@@ -19,15 +18,15 @@ module Jekyll
           @gh_data[name]['stars'] = data['stargazers_count']
           @gh_data[name]['forks'] = data['forks_count']
           i += 1
+          @gh_data['generated_time'] = Time.now
+          File.open('_data/github.yml', 'w') { |f| f.write @gh_data.to_yaml }
         else
-          timediff =  Time.now - Time.at(site.data['test']['generated_time'])
+          timediff =  Time.now - Time.at(site.data['github']['generated_time'])
           diff_min = 60 - (timediff / 60)
           binding.pry
-          abort('You maxed out the GitHub API rate limit! How could you!? try again in ' + diff_min.to_s + ' minutes.')
+          puts('You maxed out the GitHub API rate limit! How could you!? try again in ' + diff_min.to_s + ' minutes.')
         end
-        @gh_data['generated_time'] = Time.now
-        binding.pry
-        File.open('_data/github.yml', 'w') { |f| f.write @gh_data.to_yaml }
+        
         # site.data['projects'][i]['github-data'] = @gh_data
       end
     end
