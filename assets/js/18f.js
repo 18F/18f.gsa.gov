ab5a2e3cd00d1ce12534d96b4a060c55d79cce1c
Michelle Hertzfeld <michelle.hertzfeld@gsa.gov>
2014-09-22 14:32:35 -0700

    Returned slideshow to JS. Removed Sass mixin slideshow.

--- assets/js/18f.js
+++ assets/js/18f.js
@@ -37,6 +37,30 @@ var $buoop = {vs:{i:8,f:10,o:12,s:5}};
   document.body.appendChild(e);
 }
 
+
+//For slideshow. Thanks to http://responsiveslides.com/
+$(function() {
+  // $(".rslides").responsiveSlides();
+  $(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 3000,            // Integer: Speed of the transition, in milliseconds
+    timeout: 6000,          // Integer: Time between slide transitions, in milliseconds
+    pager: false,           // Boolean: Show pager, true or false
+    nav: false,             // Boolean: Show navigation, true or false
+    random: false,          // Boolean: Randomize the order of the slides, true or false
+    pause: false,           // Boolean: Pause on hover, true or false
+    pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+    prevText: "Previous",   // String: Text for the "previous" button
+    nextText: "Next",       // String: Text for the "next" button
+    maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+    navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+    manualControls: "",     // Selector: Declare custom pager navigation
+    namespace: "rslides",   // String: Change the default namespace used
+    before: function(){},   // Function: Before callback
+    after: function(){}     // Function: After callback
+  });
+});
+
 //Initial load of page
 window.onload=function() { sizeContent() };
 
