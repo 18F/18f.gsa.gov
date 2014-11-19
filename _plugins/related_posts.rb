eb5563b09bc24060bbd77a3976657e75fc6907ee
Greg Boone <gregory.boone@gsa.gov>
2014-11-19 14:43:52 -0500

    Adds a related_posts Liquid tag
    - At time of site generation, each blog post will get a list of up to 5 blog
      posts from the same tags and categories

---
+++ _plugins/related_posts.rb
@@ -0,0 +1,29 @@
+require 'pry'
+module Jekyll
+  class RelGenerator < Liquid::Tag
+
+    def initialize(related_posts, text, tokens)
+      super
+      @text = text
+    end
+
+    def render(context)
+      tags = context['page']['tags']
+      siteTags = context['site']['tags']
+      other_posts = []
+      for tag in tags
+        other_posts.push( siteTags[tag] )
+        other_posts = other_posts.flatten.uniq
+        if other_posts.size > 5
+          other_posts = other_posts.drop(5)
+        end
+      end
+      related_posts = "<ul>"
+      for post in other_posts.flatten
+        related_posts += '<li><a href="'+post.url+'" class="related_posts">' + post.title + '</a></li>'
+      end
+      related_posts += "</ul>"
+    end
+  end
+end
+Liquid::Template.register_tag('related_posts', Jekyll::RelGenerator)
