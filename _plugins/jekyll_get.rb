95f997608e6fbe13c474e09c7aa0ee8ef70e8740
Mike Bland <mbland@acm.org>
2015-03-18 14:35:58 -0400

    Update Jekyll_Get to use HashJoiner
    This ensures that any existing data from the _data directory is merged with the incoming JSON, rather than getting replaced by it. Also, `priority: highest` ensures this Generator runs first.

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -1,7 +1,11 @@
+require 'hash-joiner'
 require 'open-uri'
 
 module Jekyll_Get
   class Generator < Jekyll::Generator
+    safe true
+    priority :highest
+
     def generate(site)
       config = site.config['jekyll_get']
       if !config
@@ -10,7 +14,15 @@ module Jekyll_Get
       if !config.kind_of?(Array)
         config = [config]
       end
-      config.each { |d| site.data[d['data']] = JSON.load(open(d['json'])) }
+      config.each do |d|
+        target = site.data[d['data']]
+        source = JSON.load(open(d['json']))
+        if target
+          HashJoiner.deep_merge target, source
+        else
+          site.data[d['data']] = source
+        end
+      end
     end
   end
 end
