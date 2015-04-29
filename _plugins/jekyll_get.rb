cbce0f4b0dc7430643c081d76d8fcaa3f440cfcf
Parker Moore <parkrmoore@gmail.com>
2015-04-29 13:40:22 -0700

    Refactor jekyll_get plugin to be a bit more sane.

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -7,27 +7,38 @@ module Jekyll_Get
     priority :highest
 
     def generate(site)
-      config = site.config['jekyll_get']
-      return if !config
-      Array(config).each do |to_get|
+      sources_to_fetch = site.config['jekyll_get']
+      return if !sources_to_fetch
+
+      Array(sources_to_fetch).each do |to_get|
         begin
-          target = site.data[to_get['data']]
-          source = JSON.load(open(to_get['json']))
-          if target
-            Jekyll::Utils.deep_merge_hashes target, source
-          else
-            site.data[to_get['data']] = source
-          end
-          if to_get['cache']
-            path = "#{site.config['data_source']}/#{to_get['data']}.json"
-            open(path, 'wb') do |file|
-              file << JSON.generate(site.data[to_get['data']])
+          # Only fetch the data if it's not cached already.
+          if !(data_from_site(site, to_get) && to_get['cache'])
+            new_data = JSON.load(open(to_get['json']))
+            merge_with_preexisting_data(site, to_get, new_data)
+            if to_get['cache']
+              File.write(cache_path(site, to_get), JSON.generate(data_from_site(site, to_get)))
             end
           end
         rescue
+          $stderr.puts "Error fetching #{to_get['json']}, skipping."
           next
         end
       end
+    end
+
+    private
+    def cache_path(site, to_get)
+      "#{site.config['data_source']}/#{to_get['data']}.json"
+    end
+
+    def merge_with_preexisting_data(site, to_get, new_data)
+      preexisting = data_from_site(site, to_get) || {}
+      site.data[to_get['data']] = Jekyll::Utils.deep_merge_hashes preexisting, new_data
+    end
+
+    def data_from_site(site, to_get)
+      site.data[to_get['data']]
     end
   end
 end
