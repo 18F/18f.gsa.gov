0f1b3b5f80bce7ffa2bee9f1ef83e19087b4b6ad
Dave Cole <david.cole@gsa.gov>
2015-06-02 11:27:26 -0400

    Restructure data files as array. Adjust team landing page and plugins accordingly.

--- _plugins/pif_data.rb
+++
@@ -1,7 +0,0 @@
-module Jekyll
-  class PifData < Generator
-    def generate(site)
-      (site.data['pif_team'] || []).each {|name, data| data['name'] = name}
-    end
-  end
-end
--- _plugins/team.rb
+++ _plugins/team.rb
@@ -3,7 +3,7 @@ module Jekyll
 		# Assumes you have a data file name "team"
     def team_photo(input)
       person = finder(input)
-      image = File.join 'assets', 'images', 'team', "#{input}.jpg" 
+      image = File.join 'assets', 'images', 'team', "#{input}.jpg"
       if File.exist?(File.join(Jekyll.sites[0].config['source'], image))
         "<img class='img-circle team-img bio-clip' src='/#{image}' alt='18F team member #{person['full_name']}'>" unless person.nil?
       else
@@ -15,7 +15,7 @@ module Jekyll
     	person = finder(input)
     	person['full_name'] || person['display'] unless person.nil?
     end
-    
+
     def team_role(input)
     	person = finder(input)
     	if person['team']
@@ -28,7 +28,7 @@ module Jekyll
     end
 
     def finder(name)
-        return Jekyll.sites[0].data['team'][name]
+        return Jekyll.sites[0].data['team'].find {|member| member["name"] == name}
     end
   end
 end
