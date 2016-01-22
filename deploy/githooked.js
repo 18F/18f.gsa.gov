4348bbae9ca1c6d83ded44921df745e537515b64
Scott O'Neal <scottmod@gmail.com>
2016-01-22 14:11:00 -0500

    deploy: githooked: removing bodyParser json payload limit, integrated 1mb manually into githooked. see ScottONeal/githooked@2f894c8

--- deploy/githooked.js
+++ deploy/githooked.js
@@ -15,8 +15,7 @@ if (!program.branch || !program.command || !program.port) {
   process.exit(1);
 }
 
-// limit: Passed through to bodyParser.json(), value: 1mb.
-// // https://www.npmjs.com/package/body-parser#limit
-githooked("refs/heads/" + program.branch, program.command, { limit: 1000000 }).listen(program.port, function() {
+// Create githooked server
+githooked("refs/heads/" + program.branch, program.command).listen(program.port, function() {
   console.log("Huzzah! Listening on port " + program.port + " for push events on " + program.branch + ".");
 });
