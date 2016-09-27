183a3c3ee533f37694953ca7b13539347479ff96
Brian Hedberg <bshslowfood@gmail.com>
2016-09-27 10:01:25 -0500

    adjust bash json

--- nightly_build.sh
+++ nightly_build.sh
@@ -2,13 +2,13 @@
 _project=$1
 _branch=$2
 _circle_token=$3
-_functional_tests_target=$4
+
 trigger_build_url=https://circleci.com/api/v1/project/${_project}/tree/${_branch}?circle-token=${_circle_token}
 post_data=$(cat <<EOF
 {
   "build_parameters": {
     "RUN_NIGHTLY_BUILD": "true",
-    "FUNCTIONAL_TEST_TARGET": ${_functional_tests_target}
+    "FUNCTIONAL_TEST_TARGET": "https://federalist.18f.gov/preview/18F/18f.gsa.gov/master/"
   }
 }
 EOF)
@@ -17,4 +17,3 @@ curl \
 --header "Content-Type: application/json" \
 --data "${post_data}" \
 --request POST ${trigger_build_url}
-Add Comment Collapse
