48d6aaed9977f3cccaee2886714b6c911e283e4e
Brian Hedberg <bshslowfood@gmail.com>
2016-11-02 17:13:43 -0500

    updates to project page with styles, documentation for new match_posts method

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -3,6 +3,19 @@ require 'rb-readline'
 
 module Jekyll
   module MatchingPosts
+    # match_posts filter
+    #
+    # A liquid filter that takes a page object
+    #
+    # Example:
+    # ```
+    # {{ page | match_posts }}
+    # ```
+    #
+    # Will look for all the posts on the site
+    # and return a list of posts that have any tag
+    # that matches the list of `project_tags` defined
+    # in a given project's frontmatter
     def match_posts(page)
       matching_posts = []
       Jekyll.sites[0].posts.docs.each do |sitepost|
