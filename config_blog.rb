de8a4ceb78a2bc2b77d02a1f7f0aeda5abf484cc
Brian Hedberg <bshslowfood@gmail.com>
2016-12-05 14:49:12 -0600

    ruby syntax

--- config_blog.rb
+++ config_blog.rb
@@ -3,23 +3,22 @@ require 'rb-readline'
 # script to update the blog build config
 posts = './_posts'
 post_directory = Dir[File.join(posts, '**', '*')]
-post_count = post_directory.count { |file| File.file?(file) }
 stripped_post_directory = post_directory.map do |post|
   post.slice(2, post.size)
 end
 
+post_count = stripped_post_directory.count
 config_path = './_config-blog.yml'
 lines = File.readlines(config_path)
 acceptable_indices = [
-  stripped_post_directory.count - 1,
-  stripped_post_directory.count - 2,
-  stripped_post_directory.count - 3 ]
-
+  post_count - 1,
+  post_count - 2,
+  post_count - 3
+]
 
 stripped_post_directory.each_with_index do |post, index|
-  if !lines.include? "- #{post}\n" and !acceptable_indices.include? index
-      lines << "- #{post}"
-  end
+  excluded_post = !lines.include? "- #{post}\n" and !acceptable_indices.include? index
+  lines << "- #{post}" if excluded_post
 end
 
 File.open(config_path, 'w') do |f|
