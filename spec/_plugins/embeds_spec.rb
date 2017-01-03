59218eda660fd0e12f2bf51c1b616aa6c3a180ae
Brian Hedberg <bshslowfood@gmail.com>
2017-01-03 13:13:44 -0600

    embed plugin

--- spec/_plugins/embeds_spec.rb
+++ spec/_plugins/embeds_spec.rb
@@ -1,9 +1,15 @@
 require_relative '../../_plugins/embeds'
 
 RSpec.describe Jekyll::EmbedFilter do
+  class EmbedFilterClass
+  end
+
+  before(:each) do
+    @embed_filter = EmbedFilterClass.new
+    @embed_filter.extend(Jekyll::EmbedFilter)
+  end
   it 'takes returns an embed given one variable' do
-    embed_filter = Jekyll::EmbedFilter
-    actual = embed_filter.embed('/url/')
+    actual = @embed_filter.embed('/url/')
 
     expected = "<div class='embed-container'><iframe src='/url/' title='/url/'" \
         " width='560' height='315' frameborder='0' allowfullscreen></iframe></div>"
@@ -11,8 +17,7 @@ RSpec.describe Jekyll::EmbedFilter do
   end
 
   it 'takes returns an embed given two variable' do
-    embed_filter = Jekyll::EmbedFilter
-    actual = embed_filter.embed('/url/', 'url title')
+    actual = @embed_filter.embed('/url/', 'url title')
 
     expected = "<div class='embed-container'><iframe src='/url/' title='url title'" \
         " width='560' height='315' frameborder='0' allowfullscreen></iframe></div>"
