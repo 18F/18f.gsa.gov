ae608fa05bb71673127953851b4352308b2742b4
Andrew Burnes <andrew.burnes@gsa.gov>
2020-02-20 16:26:19 -0700

    update header

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,13 +11,13 @@ $(function (){
   });
 
   // Drawer
-  $('.menu-btn, .overlay, .sliding-panel-close').on('click touchstart', function (e) {
+  $('.menu-btn, .overlay, .sliding-panel-close, .usa-nav__close').on('click touchstart', function (e) {
     $('.nav-mobile, .overlay').toggleClass('is-visible');
     e.preventDefault();
   });
 
   // Styleguide drawer
-  $('.menu-btn-styleguide, .sliding-panel-close-styleguide').on('click touchstart', function (e) {
+  $('.menu-btn-styleguide, .sliding-panel-close-styleguide, .usa-nav__close').on('click touchstart', function (e) {
     $('.nav-mobile-styleguide').toggleClass('is-visible');
     e.preventDefault();
   });
