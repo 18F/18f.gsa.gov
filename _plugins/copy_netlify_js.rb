ed3321e5aa611a61b14441eb361690f9caddfc71
Laura Gerhardt <laura.gerhardt@gsa.gov>
2021-02-23 16:34:05 -0500

    fix houndci

--- _plugins/copy_netlify_js.rb
+++ _plugins/copy_netlify_js.rb
@@ -3,8 +3,7 @@ require 'fileutils'
 netlify_src = './node_modules/netlify-cms/dist/netlify-cms.js'
 netlify_dest = './assets/js/netlify-cms.js'
 
-Jekyll::Hooks.register :site, :after_init do ||
+Jekyll::Hooks.register :site, :after_init do
   # code to call after Jekyll renders a page
   FileUtils.cp(netlify_src, netlify_dest)
 end
-
