bf48961a3c119dd5b37973ef38e0fe1c2c23a216
Matt J Cloyd <matt.cloyd@gsa.gov>
2024-04-22 16:20:14 -0400

    Add site-wide pa11y checking
    This commit adds site-wide pa11y checking. If a "global" folder (like _includes or _sass) sees a file change, we add a few files from several folders across the site, and include them in the list of files for pa11y to check.
    
    This also switches file "differs" based on the environment. If CI=true, then we diff the files from the last commit. Otherwise, we assume we're developing locally and get the files currently changed.
    
    This feels like draft code, the design feels off. There's constants set at file load/runtime, and with the new class involved, the file is a lot harder to read.

--- _plugins/pa11y.rb
+++ _plugins/pa11y.rb
@@ -29,38 +29,140 @@ Document = Struct.new(:path, :layout, :differ) do
   end
 
   def source_changed?
-    differ.changed_files.include?(path)
+    changed_files.include?(path)
   end
 
   def layout_changed?
-    differ.changed_files.include?("_layouts/#{layout}.html")
+    changed_files.include?("_layouts/#{layout}.html")
+  end
+
+  # Memoize these without having to memoize in the differ
+  def changed_files
+    @changed_files ||= differ.changed_files
+  end
+end
+
+class Differ
+  # @return [Array<String>] List of relative paths of files
+  def self.changed_files
+    @changed_files ||= %x[ #{list_files_command} ].to_s.split("\n").map(&:strip)
+  end
+
+  # @return [String] Text of a shell command to get listed files
+  def self.list_files_command
+    "git ls-files --modified"
   end
 end
 
 # Gets the list of files changed in the last commit
-# @note Designed as a collaborator object to make Document easier
-#   to test
-module GitDiffer
-  # @return [Array<String>] List of relative paths of files that were
-  #   changed in the last commit.
-  def self.changed_files
-    @changed_files ||= %x[ git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD) ]
-      .to_s
-      .split("\n")
-      .map(&:strip)
+class CommitDiffer < Differ
+  def self.list_files_command
+    "git diff-tree --no-commit-id --name-only -r $(git rev-parse HEAD)"
   end
 end
 
-# Outputs posts and pages to scan in file `pa11y_targets`.
+# Jekyll::Document#destination requires a root path parameter
+# This constant replaces a mysterious empty string when
+# calling #destination.
+NO_ROOT_PATH = "".freeze
+
+PA11Y_TARGET_FILE = ENV.fetch('PA11Y_TARGET_FILE') { 'pa11y_targets' }
+
+# Changes to files in these directories trigger a global check
+# @todo Is there a way to grab these more dynamically?
+SITEWIDE_FOLDERS = ["_includes", "_sass"]
+
+DIFFER = ENV.fetch("CI", false) ? CommitDiffer : Differ
+
+# Outputs posts and pages to scan to a file.
 # @todo Implement the stylesheet checker (third bullet above)
 Jekyll::Hooks.register :documents, :post_render do |doc|
-  puts doc.relative_path if doc.relative_path.match?(/css/)
   document = Document.new(
     doc.relative_path,
     doc.data["layout"],
-    GitDiffer
+    DIFFER
   )
   if document.to_scan?
-    File.open('pa11y_targets', 'a') { |f| f.write doc.destination("") }
+    File.open(PA11Y_TARGET_FILE, 'a') { |f|
+      f.write(doc.destination(NO_ROOT_PATH) + "\n")
+    }
+  end
+end
+
+# Collects
+# @todo This is first-draft working code, I don't feel settled about
+#   the design.
+class PageCollector
+
+  attr_accessor :config
+
+  def initialize(config)
+    @config = config
+  end
+
+  def pages
+    sampled_collection_paths + sampled_blog_archive_paths
+  end
+
+  # Sample 3 files from each collection
+  def sampled_collection_paths
+    collection_paths.map { |path| sample_folder(path) }.flatten.uniq
+  end
+
+  def sample_folder(path)
+    Dir.glob("_site" + path + "/**/*")
+      .reject { |f| File.directory?(f) }
+      .sample(3)
+  end
+
+  # All of the paths to collections in the site config
+  # @todo The /styleguide collection can probably be deleted
+  def collection_paths
+    paths = config["collections"].each_pair.map do |name, data|
+      # Get the folder path from the permalink
+      data.fetch("permalink").split("/").first(2).join("/")
+    end
+    paths.delete("/:path")
+    paths.delete("/styleguide")
+    paths.uniq
+  end
+
+  # Sample first, last, and random middle page of blog archive
+  # @smell Mutating AND a while loop? At the same time??
+  def sampled_blog_archive_paths
+    get_integer(blog_pages.last) / 2
+    [blog_pages.first, blog_pages.last, middle_blog_archive_page]
+  end
+
+  def middle_blog_archive_page
+    middle_page_num = get_integer(blog_pages.last) / 2
+    finder = Regexp.new("/#{middle_page_num}/")
+    blog_pages.detect { |x| x.match? finder }
+  end
+
+  # Blog archive page paths, sorted by page number (/blog/page/:number)
+  def blog_pages
+    @blog_pages ||= Dir.glob("_site/blog/page/**/*.html")
+      .sort_by { |path| get_integer(path) }
+  end
+
+  def get_integer(text)
+    text.match(/(\d+)/).captures.first.to_i
+  end
+end
+
+# If files have changed in any global directories, sample folders
+# across the site and add them to the list of files to check with
+# pa11y for this PR
+# @todo Is there a way to unnest this code? Jekyll hooks don't allow
+#   early returns.
+Jekyll::Hooks.register :site, :post_render do |site|
+  global_files = Regexp.new SITEWIDE_FOLDERS.map { |x| "^#{x}" }.join("|")
+  if DIFFER.changed_files.grep(global_files).any?
+    File.open(PA11Y_TARGET_FILE, 'a') do |f|
+      PageCollector.new(site.config).pages.each do |path|
+        f.write(path + "\n")
+      end
+    end
   end
 end
