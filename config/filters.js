884acc16f664f624818ab8420657ef783531aa06
Matt J Cloyd <matt.cloyd@gsa.gov>
2024-06-03 10:26:44 -0400

    Finish migrating authors
    This commit:
    - Removes "author published" status checks
    - Re-implements team_link and team_photo filters
    - Re-implements an "authors' matching posts" filter.
    - Refactors the author post display
      - Refactors layout into partials
      - Refactors various author / post display logic
      - Removes unnecessary (hopefully) context mutation
    - Fixes author data
      - Adds missing team member data
      - Renames authors.csv -> team_members.csv because global data was polluting post.data.authors
    
    The consequence of removing the `published` is that we'll have 63 pages for unpublished authors, most/all of whom are team members featured in the "we asked 100 colleagues" posts.
    
    These pages are unlinked so no one is likely to stumble upon them by accident. Perhaps we can add a robots.txt entry for them later so they're not indexed.
    
    This doesn't have a meaningful impact on performance.

--- config/filters.js
+++ config/filters.js
@@ -1,6 +1,8 @@
 // Keep filter source code here.
-
+const fs = require('fs');
+const { parse } = require('csv-parse/sync');
 const { DateTime } = require('luxon');
+const { imageShortcode, imageWithClassShortcode } = require('./index');
 
 const readableDate = (dateObj) => DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
   'dd LLL yyyy',
@@ -31,9 +33,63 @@ const embed = (url, title=url) => {
   return `<div class='embed-container'><iframe src='${url}' title='${title}' width='560' height='315' frameborder='0' allowfullscreen></iframe></div>`
 }
 
+const teamData = parse(
+  fs.readFileSync('./_data/team_members.csv', 'utf8'), {
+    columns: true,
+    skip_empty_lines: true
+  }
+);
+
+const findPerson = (id) => {
+  const maybePerson = teamData.find((person) => person.id == id);
+  if (maybePerson === undefined) {
+    console.log(id, "is undefined")
+  }
+  return maybePerson
+}
+
+const fullName = (person) => {
+  if (person.full_name != '') {
+    return person.full_name;
+  } else {
+    return `${person.first_name} ${person.last_name}`
+  }
+}
+
+const team_photo = (slug) => {
+  const maybePerson = findPerson(slug)
+  const maybeImage = `assets/img/team/${slug}.jpg`
+  var imagePath;
+  var caption;
+  var placeholderImage;
+
+  if (fs.existsSync(maybeImage)) {
+    imagePath = maybeImage;
+    placeholderImage = false;
+  } else {
+    imagePath = "assets/img/logos/18F-Logo-M.png";
+    placeholderImage = true;
+  }
+
+  if (maybePerson) {
+    if (placeholderImage) {
+      caption = `Placeholder image for 18F team member ${fullName(maybePerson)} (18F logo)`;
+    } else {
+      caption = `Photo of 18F team member ${fullName(maybePerson)}`;
+    }
+  } else {
+    caption = "Placeholder photo for 18F team member";
+  }
+
+  return imageShortcode(imagePath, caption);
+}
+
+const team_link = (slug) => {
+  const name = fullName(findPerson(slug))
+  return `<a href="/author/${slug}">${name}</a>`
+}
+
 // TODO These all need implementation, they're just placeholders so the site builds at all
-const team_photo = (slug) => { return `TODO PHOTO FOR ${slug}` }
-const team_link = (slug) => { return `TODO LINK FOR ${slug}` }
 const find_collection = (site, collection) => { return [] }
 const weighted_sort = (array, weight_name, sort_name) => { return array }
 const in_groups = (array, groups) => { return array }
